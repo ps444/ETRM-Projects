0ac138f372ce1db89d1dc6135b129e197c50e595
Tolu Ayelabola <tayelabola@bsptechnologies.co.uk>
2025-09-11 14:32:04 +0100

    Update README with project details

---
+++ main.py
@@ -0,0 +1,17 @@
+import random;
+import sys;
+
+num = int(sys.argv[1])
+
+
+def generate_random_number(parameters):
+    
+    for counter in range(parameters):
+        print(round(random.uniform(0, 100), 2))
+
+
+
+
+
+print( generate_random_number(num))
+
